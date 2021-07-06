d97297643837b6f33a09f66f4e7465e982e80d4b
Magdalena Ladron de Guevara <82461941+Maguimages@users.noreply.github.com>
2021-07-06 15:46:05 -0400

    Add files via upload

--- SQL/tarea_countries.sql
+++ SQL/tarea_countries.sql
@@ -26,12 +26,51 @@ SELECT countries.name, count(cities.name)
 	GROUP BY countries.id DESC 
         ORDER BY count(cities.name) DESC;
         
-        -- ¿Qué consulta harías para obtener todas las ciudades de México con una población de más de 500,000? 
+        -- 3. ¿Qué consulta harías para obtener todas las ciudades de México con una población de más de 500,000? 
         -- Tu consulta debe organizar el resultado por población en orden descendente. (1)
  SELECT countries.name, cities.name
 	FROM countries
 	JOIN cities ON countries.id = cities.country_id
 	WHERE cities.population > 500000 AND countries.name LIKE 'Mexico';
+    
+   -- 4. ¿Qué consulta ejecutarías para obtener todos los idiomas en cada país con un porcentaje superior al 89%? 
+   -- Tu consulta debe organizar el resultado por porcentaje en orden descendente. (1)
+	
+	SELECT countries.name, languages.language, languages.percentage
+	FROM countries
+	LEFT JOIN languages ON countries.id = languages.country_id
+	WHERE percentage > 89
+	ORDER BY percentage DESC;
+
+-- ¿Qué consulta haría para obtener todos los países con un área de superficie inferior a 501 y una población superior a 100,000? (2)
+
+	SELECT name, surface_area, population FROM countries
+	WHERE surface_area < 501 AND population > 100000;
+
+-- 6. ¿Qué consulta harías para obtener países con solo Monarquía Constitucional con un capital superior a 200 y
+--  una esperanza de vida superior a 75  años? (1)
+
+	SELECT name, government_form, capital, life_expectancy FROM countries
+	WHERE government_form LIKE 'constitutional monarchy' AND capital > 200 AND life_expectancy > 75;
+
+-- 7. ¿Qué consulta harías para obtener todas las ciudades de Argentina dentro del distrito de Buenos Aires y tener una población superior a 500,000? 
+-- La consulta debe devolver el nombre del país, el nombre de la ciudad, el distrito y la población. (2)
+
+	SELECT countries.name AS país, cities.name AS ciudad , cities.district as distrito, cities.population as poblacion
+	FROM countries
+	LEFT JOIN cities ON countries.id = cities.country_id
+	WHERE cities.district LIKE 'Buenos Aires' AND cities.population > 500000;
+    
+    SELECT *
+
+-- 8. ¿Qué consulta harías para resumir el número de países en cada región? 
+-- La consulta debe mostrar el nombre de la región y el número de países. 
+-- Además, la consulta debe organizar el resultado por el número de países en orden descendente.
+	select * from countries;
+    
+	SELECT region, count(region) AS num_paises FROM countries
+	GROUP BY region 
+	ORDER BY num_paises DESC;
 
 
 
